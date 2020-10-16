6913b61b7304dcbe1bbd5afe42129ffd0723fb5b
ITS | Aaron Levi Can <aaron@its-wurst.de>
2020-10-16 12:04:06 +0200

    added 'ls' to basics.py

--- levish/commands/basics.py
+++ levish/commands/basics.py
@@ -1,4 +1,9 @@
 from os import system as _exec
+from os import listdir as _listdir
+from os import getcwd as _getcwd
+from os.path import isdir as _isdir
+from os.path import join as _join
+
 from platform import system as _system
 
 def clear(args):
@@ -6,3 +11,15 @@ def clear(args):
         _exec("cls")
     else:
         _exec("clear")
+
+def ls(args):
+    if len(args) == 0:
+        for i in _listdir():
+            if _isdir(_join(_getcwd(), i)):
+                print(f"{i} ..")
+            else:
+                print(f"{i} .")
+    elif len(args) == 1:
+        pass
+    else:
+        print("Too much arguments")
