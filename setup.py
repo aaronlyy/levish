b96087991fba3a398751f611b6a19ab30d45dc63
ITS | Aaron Levi Can <aaron@its-wurst.de>
2020-11-12 13:21:09 +0100

    setup.py edit

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 setup(
   name = 'levish',
   packages = ['levish', 'levish.commands'],
-  version = '0.1.8.0',
+  version = '0.1.8.1',
   license='GPL-3.0 License',
   description = 'Create your own shell.',
   long_description = _long_description,
@@ -15,7 +15,7 @@ setup(
   author = 'Aaron Levi Can (aaronlyy)',
   author_email = 'aaronlevican@gmail.com',
   url = 'https://github.com/aaronlyy/levish',
-  download_url = 'https://github.com/aaronlyy/levish/archive/v0.1.8.0.tar.gz',
+  download_url = 'https://github.com/aaronlyy/levish/archive/v0.1.8.1tar.gz',
   keywords = ['shell', 'terminal', 'commands', 'cmd', 'console'],
   install_requires=[
         "pyfiglet"
